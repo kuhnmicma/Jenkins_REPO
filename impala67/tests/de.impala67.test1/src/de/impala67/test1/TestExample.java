6d25175097a0225c3862ea3be98ef4273be7bfb7
impala67 <michi.kuhn4@yahoo.de>
2017-11-17 12:58:57 +0100

    helloWorld

---
+++ impala67/tests/de.impala67.test1/src/de/impala67/test1/TestExample.java
@@ -0,0 +1,13 @@
+package de.impala67.test1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestExample {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
